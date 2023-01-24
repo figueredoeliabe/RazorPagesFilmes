2bb650586a6e46edc876a0de11f8f519f7c9b415
figue <figue@DESKTOP-4E4NTTS>
2023-01-24 17:49:47 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesFilmes/RazorPagesFilmes/Migrations/20230124183651_adicionando dataanotations.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RazorPagesFilmes.Migrations
+{
+    public partial class adicionandodataanotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Filme",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Filme",
+                type: "decimal(18, 2",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genero",
+                table: "Filme",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Filme",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Preco",
+                table: "Filme",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genero",
+                table: "Filme",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(25)",
+                oldMaxLength: 25);
+        }
+    }
+}
---
+++ RazorPagesFilmes/RazorPagesFilmes/Model/Filme.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesFilmes.Model
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+        
+        [StringLength(100, MinimumLength =1 )]
+        [MaxLength(100, ErrorMessage ="O nome do filme deve conter entre 1 e 100 carácteres.")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required(ErrorMessage ="Gênero obrigatório")]
+        [StringLength(25, MinimumLength =2)]
+        public string Genero { get; set; } = string.Empty;
+        
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2")]
+        public decimal Preco { get; set; }
+
+        [Range(0,10)]
+        public int Avaliacao { get; set; } = 0;
+    }
+}
